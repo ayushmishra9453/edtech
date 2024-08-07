b0275c4b556c0fa299c6d3960943245046ba1c8b
ayushmishra9453 <ayushmishra9453@gmail.com>
2024-08-08 01:18:51 +0530

    category controller commit

--- backend/controllers/Categorys.js
+++ backend/controllers/Categorys.js
@@ -56,30 +56,66 @@ exports.categoryPageDetails=async(req,res)=>{
 
     // fetch all courses with that category id
     const selectedCategory=await Category.find(categoryId)
-    .populate("courses")
+    .populate({
+      path: "courses",
+      match: { status: "Published" },
+      populate: "ratingAndReviews",
+    })
     .exec();
     //  validation
     if(!selectedCategory){
+      console.log("Category not found.")
       return res.status(404).json({
         success:false,
         message:"Data not found"
       })
     }
 
-    // get courses for different categories
-    const differentCategory=await Category.find({
-      _id:{$ne:categoryId}
+    // Handle the case when there are no courses
+    if (selectedCategory.courses.length === 0) {
+      console.log("No courses found for the selected category.")
+      return res.status(404).json({
+        success: false,
+        message: "No courses found for the selected category.",
+      })
+    }
+
+    // Get courses for other categories
+    const categoriesExceptSelected = await Category.find({
+      _id: { $ne: categoryId },
     })
-    .populate("courses")
+
+    // get courses for different categories
+    let differentCategory=await Category.findOne(
+      categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+      ._id
+    )
+    .populate({  path: "courses",
+      match: { status: "Published" },})
     .exec();
     // homework--> get top selling courses
-    
+       // Get top-selling courses across all categories
+       const allCategories = await Category.find()
+       .populate({
+         path: "courses",
+         match: { status: "Published" },
+         populate: {
+           path: "instructor",
+       },
+       })
+       .exec()
+     const allCourses = allCategories.flatMap((category) => category.courses)
+     const mostSellingCourses = allCourses
+       .sort((a, b) => b.sold - a.sold)
+       .slice(0, 10)
+      // console.log("mostSellingCourses COURSE", mostSellingCourses)
   //  return response
   return res.status(200).json({
     success:true,
     data:{
       selectedCategory,
-      differentCategory
+      differentCategory,
+      mostSellingCourses,
     },
   });
   }
