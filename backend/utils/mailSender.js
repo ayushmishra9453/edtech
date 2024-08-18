54a241da160cc0f3f9b547e18b878a8b22495bb9
ayushmishra9453 <ayushmishra9453@gmail.com>
2024-08-18 21:41:16 +0530

    otp changes

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -1,29 +1,56 @@
 const nodemailer = require("nodemailer");
 
+// const mailSender = async (email, title, body) => {
+//     try{
+//             let transporter = nodemailer.createTransport({
+//                 host:process.env.MAIL_HOST,
+//                 port: process.env.SMPT_PORT,
+//                 service: process.env.SMPT_SERVICE,
+//                 auth:{
+//                     user: process.env.MAIL_USER,
+//                     pass: process.env.MAIL_PASS,
+//                 }
+//             })
+
+
+//             let info = await transporter.sendMail({
+//                 from: 'StudyNotion || CodeHelp - by Babbar',
+//                 to:`${email}`,
+//                 subject: `${title}`,
+//                 html: `${body}`,
+//             })
+//             console.log(info);
+//             return info;
+//     }
+//     catch(error) {
+//         console.log(error.message);
+//     }
+// }
+
+
+// module.exports = mailSender;
+
 const mailSender = async (email, title, body) => {
-    try{
-            let transporter = nodemailer.createTransport({
-                host:process.env.MAIL_HOST,
-                auth:{
-                    user: process.env.MAIL_USER,
-                    pass: process.env.MAIL_PASS,
-                }
-            })
-
-
-            let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
-                to:`${email}`,
-                subject: `${title}`,
-                html: `${body}`,
-            })
-            console.log(info);
-            return info;
+    try {
+      let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      });
+  
+      let info = await transporter.sendMail({
+        from: 'StudyNotion || CodeHelp - by Babbar',
+        to: `${email}`,
+        subject: `${title}`,
+        html: `${body}`,
+      });
+  
+      return { response: info.response };
+    } catch (error) {
+      console.log(error.message);
+      return { error: error.message };
     }
-    catch(error) {
-        console.log(error.message);
-    }
-}
-
-
-module.exports = mailSender;
+  };
+  module.exports=mailSender;
